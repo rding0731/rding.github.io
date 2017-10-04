3ee766491a73956c378fde663e9101d275c339b0
rding0731 <dingruonan@gmail.com>
2017-10-04 01:43:11 -0400

    push1

--- Reinforman_Learning/GridSearch/gridSearch_setup.py
+++ Reinforman_Learning/GridSearch/gridSearch_setup.py
@@ -28,24 +28,21 @@ import io
 
 from gym.envs.toy_text import discrete
 
-UP = 0
-RIGHT = 1
-DOWN = 2
-LEFT = 3
-
-
+RIGHT = 0
+DOWN = 1
 
 class GridworldEnv(discrete.DiscreteEnv):
 
     metadata = {'render.modes': ['human', 'ansi']}
-    def __init__(self, shape=[4,4]):
+    
+    def __init__(self, shape=[2,15]):
         if not isinstance(shape, (list, tuple)) or not len(shape) == 2:
             raise ValueError('shape argument must be a list/tuple of length 2')
 
         self.shape = shape
 
         nS = np.prod(shape)  ## total elements in the matrix
-        nA = 4
+        nA = 15
 
         MAX_Y = shape[0]
         MAX_X = shape[1]
@@ -65,20 +62,15 @@ class GridworldEnv(discrete.DiscreteEnv):
 
             # We're stuck in a terminal state
             if is_done(s):
-                P[s][UP] = [(1.0, s, reward, True)]
                 P[s][RIGHT] = [(1.0, s, reward, True)]
                 P[s][DOWN] = [(1.0, s, reward, True)]
-                P[s][LEFT] = [(1.0, s, reward, True)]
+                
             # Not a terminal state
             else:
-                ns_up = s if y == 0 else s - MAX_X
                 ns_right = s if x == (MAX_X - 1) else s + 1
                 ns_down = s if y == (MAX_Y - 1) else s + MAX_X
-                ns_left = s if x == 0 else s - 1
-                P[s][UP] = [(1.0, ns_up, reward, is_done(ns_up))]
                 P[s][RIGHT] = [(1.0, ns_right, reward, is_done(ns_right))]
                 P[s][DOWN] = [(1.0, ns_down, reward, is_done(ns_down))]
-                P[s][LEFT] = [(1.0, ns_left, reward, is_done(ns_left))]
 
             it.iternext()
 
--- Reinforman_Learning/GridSearch/policy_evaluation.py
+++ Reinforman_Learning/GridSearch/policy_evaluation.py
@@ -17,19 +17,31 @@ Value Function V(s) can only show how good it is for an agent to be in a particu
 Note that the value function is specific to a given policy. 
 ''' 
 
-cd C:\Users\ruonan.ding\Documents\Github\rding.github.io\Reinforman_Learning\GridSearch
+##cd C:\Users\ruonan.ding\Documents\Github\rding.github.io\Reinforman_Learning\GridSearch
 ## This is to import the gridSearch_setup
                                         
 import numpy as np
 import gridSearch_setup
 
+env = gridSearch_setup.GridworldEnv()
+env.nS
 
-def policy_evaluation(policy, env, discount_factor = 1.0, theta = 1e-4):
+def policy_evaluation(policy, env, discount_factor = 0.75, theta = 1e-4):
     """
+    Evaluate a policy given an environment and a full description of the environment's dynamics.
+    
+    Args:
+        policy: [S, A] shaped matrix representing the policy.
+        env: OpenAI env. env.P represents the transition probabilities of the environment.
+            env.P[s][a] is a (prob, next_state, reward, done) tuple.
+        theta: We stop evaluation one our value function change is less than theta for all states.
+        discount_factor: lambda discount factor.
+    
+    Returns:
+        Vector of length env.nS representing the value function.
     """
     
-    V = np.zeros(env.nS) # Start with a random all 0 value function. In this case
-
+    V = np.zeros(env.nS)
     while True:
         delta = 0
         for s in range(env.nS): ## for each state, we are performing a full backup
@@ -39,7 +51,7 @@ def policy_evaluation(policy, env, discount_factor = 1.0, theta = 1e-4):
                     
                     v += action_prob * prob * (reward + discount_factor * V[next_state]) ## calculate the expected value
                     
-                    print(s, a, action_prob, prob, next_state, reward, done, v)
+#                    print(s, a, action_prob, prob, next_state, reward, done, v)
             delta = max(delta, np.abs(v-V[s])) ## how much the value function changes in this one full backup
             V[s] = v    
        
@@ -52,8 +64,161 @@ def policy_evaluation(policy, env, discount_factor = 1.0, theta = 1e-4):
 test case: by giving a policy for it to evaluate. 
 '''
 random_policy = np.ones([env.nS, env.nA]) / env.nA
-policy_evaluation(random_policy, env)
+v=policy_evaluation(random_policy, env)
         
+
+print("Value Function:")
+print(v)
+print("")
+
+print("Reshaped Grid Value Function:")
+print(v.reshape(env.shape))
+print("")
         
-        
+
+# Test: Make sure the evaluated policy is what we expected
+#expected_v = np.array([0, -14, -20, -22, -14, -18, -20, -20, -20, -20, -18, -14, -22, -20, -14, 0])
+#np.testing.assert_array_almost_equal(v, expected_v, decimal=2)
+
+
+
+'''
+Policy Iteration
+'''
+def policy_improvement(env, policy_eval_fn=policy_evaluation, discount_factor=0.7):
+    """
+    Policy Improvement Algorithm. Iteratively evaluates and improves a policy
+    until an optimal policy is found.
     
+    Args:
+        env: The OpenAI envrionment.
+        policy_eval_fn: Policy Evaluation function that takes 3 arguments:
+            policy, env, discount_factor.
+        discount_factor: Lambda discount factor.
+        
+    Returns:
+        A tuple (policy, V). 
+        policy is the optimal policy, a matrix of shape [S, A] where each state s
+        contains a valid probability distribution over actions.
+        V is the value function for the optimal policy.
+        
+    """
+    # Start with a random policy
+    policy = np.ones([env.nS, env.nA]) / env.nA  ## this gives an output of 16 * 4 of 1/4.
+    
+    counter = 0               
+                    
+    while True:
+        # Evaluate the current policy
+        V = policy_eval_fn(policy, env, discount_factor)
+        
+        # Will be set to false if we make any changes to the policy
+        policy_stable = True
+        
+        
+        # For each state...
+        for s in range(env.nS): # 16 states
+            
+            # The best action we would take under the current policy
+            chosen_a = np.argmax(policy[s]) ## this will start of all equal value of 0.25
+            
+            # Find the best action by one-step lookahead
+            # Ties are resolved arbitarily
+            action_values = np.zeros(env.nA) ## [0,0,0,0] 
+            for a in range(env.nA):
+                for prob, next_state, reward, done in env.P[s][a]:
+                    action_values[a] += prob * (reward + discount_factor * V[next_state])
+            best_a = np.argmax(action_values) ## Return the max index 
+            
+            # Greedily update the policy
+            if chosen_a != best_a:
+                policy_stable = False
+                counter += 1
+            policy[s] = np.eye(env.nA)[best_a] ## create a 4* 4 diagonally and then pick an index
+        
+        # If the policy is stable we've found an optimal policy. Return it
+        if policy_stable:
+            return policy, V, counter
+  
+'''optimal policy'''
+policy, v, iteration = policy_improvement(env)
+iteration
+
+print("Policy Probability Distribution:")
+print(policy)
+print("")
+
+print("Reshaped Grid Policy (0=up, 1=right, 2=down, 3=left):")
+print(np.reshape(np.argmax(policy, axis=1), env.shape))
+print("")
+
+print("Value Function:")
+print(v)
+print("")
+
+print("Reshaped Grid Value Function:")
+print(v.reshape(env.shape))
+print("")
+
+
+'''
+Value iteration
+'''
+def value_iteration(env, theta=0.0001, discount_factor=1.0):
+    """
+    Value Iteration Algorithm.
+    
+    Args:
+        env: OpenAI environment. env.P represents the transition probabilities of the environment.
+        theta: Stopping threshold. If the value of all states changes less than theta
+            in one iteration we are done.
+        discount_factor: lambda time discount factor.
+        
+    Returns:
+        A tuple (policy, V) of the optimal policy and the optimal value function.
+    """
+    
+    def one_step_lookahead(state, V):
+        """
+        Helper function to calculate the value for all action in a given state.
+        
+        Args:
+            state: The state to consider (int)
+            V: The value to use as an estimator, Vector of length env.nS
+        
+        Returns:
+            A vector of length env.nA containing the expected value of each action.
+        """
+        A = np.zeros(env.nA)
+        for a in range(env.nA):
+            for prob, next_state, reward, done in env.P[state][a]:
+                A[a] += prob * (reward + discount_factor * V[next_state])
+        return A
+    
+    V = np.zeros(env.nS)
+    while True:
+        # Stopping condition
+        delta = 0
+        # Update each state...
+        for s in range(env.nS):
+            # Do a one-step lookahead to find the best action
+            A = one_step_lookahead(s, V)
+            best_action_value = np.max(A)
+            # Calculate delta across all states seen so far
+            delta = max(delta, np.abs(best_action_value - V[s]))
+            # Update the value function
+            V[s] = best_action_value        
+        # Check if we can stop 
+        if delta < theta:
+            break
+    
+    # Create a deterministic policy using the optimal value function
+    policy = np.zeros([env.nS, env.nA])
+    for s in range(env.nS):
+        # One step lookahead to find the best action for this state
+        A = one_step_lookahead(s, V)
+        best_action = np.argmax(A)
+        # Always take the best action
+        policy[s, best_action] = 1.0
+    
+    return policy, V
